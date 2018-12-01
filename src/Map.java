30def064c2b0085d9f1fb9b841f5329d3647d8a1
Connor Grace <connor3549@gmail.com>
2018-12-01 11:01:25 -0700

    1

---
+++ src/Map.java
@@ -0,0 +1,15 @@
+public class Map {
+
+    public Map(){
+
+    }
+
+    // Build map of inputed size
+    public Node[][] buildMap(int size){
+        Node[][] map = new Node[size][size];
+
+        return map;
+    }
+
+
+}
